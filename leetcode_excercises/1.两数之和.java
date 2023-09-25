b60d5c5355b62d53f29deca108c6eca07312f8a0
ouyang-hx <ouyang-hx@outlook.com>
2023-09-25 22:01:30 +0800

    1

--- leetcode_excercises/1.两数之和.java
+++ leetcode_excercises/1.两数之和.java
@@ -5,9 +5,19 @@
  */
 
 // @lc code=start
+
+import java.util.Arrays;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
     }
 }
 // @lc code=end
