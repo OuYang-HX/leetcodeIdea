b2b4fbd66884361f26d35f7ede77aad1a9c46ed1
OYHX <92429160+OuYang-HX@users.noreply.github.com>
2023-11-09 00:52:28 +0800

    Create TestContent.md

--- leetcode/editor/cn/LC5LongestPalindromicSubstring.java
+++ leetcode/editor/cn/LC5LongestPalindromicSubstring.java
@@ -41,7 +41,7 @@
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestPalindrome(String s) {
-
+        return "";
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
